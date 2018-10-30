3b3662446b6b4d17eebb780e8971fcfcb5843bc8
Celyix002 <carolynemwende633@gmail.com>
2018-10-30 11:33:54 +0000

    changed settings and blog directory

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+
 import os
+#SECRET_KEY = os.environ['SECRET_KEY']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,6 +26,11 @@ SECRET_KEY = '(*=yixbd900v7#oe4$(_*_^-m9qx%c@ia_%hst+fce*5+ekk7$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
+#added these settings
+#CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
+
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
